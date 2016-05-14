878b71a2109ce1e784ebc866dc45f35e9bac2433
AsukaNakagawa <nina-jamie.elle14@live.jp>
2016-05-14 12:19:26 -0700

    my initial commit
    LHL-W2D2

---
+++ LHL-TutorialAutoLayout_pt1/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  LHL-TutorialAutoLayout_pt1
+//
+//  Created by Asuka Nakagawa on 2016-05-14.
+//  Copyright © 2016 Asuka Nakagawa. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UIView *topLeftView;
+@property (weak, nonatomic) IBOutlet UIView *topRightView;
+@property (weak, nonatomic) IBOutlet UIView *bottomView;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
